a57a5a737eeb81e49ba28fd9b88b8152b44162d7
Chris Apolzon <apolzon@gmail.com>
2011-04-13 20:33:04 -0700

    had to copy all required source into one file for thor

--- lib/muxilla/muxinate.rb
+++ lib/muxilla/muxinate.rb
@@ -1,7 +1,39 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'mux'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'muxinator'))
-
 module Muxilla
+  class Muxinator < Thor::Group
+    include Thor::Actions
+    argument :mux, :required => false
+
+    def self.source_root
+      File.dirname __FILE__
+    end
+
+    def create_tmux_config
+      template 'templates/tmux_config.tt', '~/tmux_config.tmux', :verbose => false
+    end
+  end
+
+  class Mux
+    attr_accessor :nickname, :type, :update, :id, :code, :rails, :resque
+    def initialize nickname, options
+      @nickname = nickname
+      @type = options[:type]
+      @update = options[:update]
+      @id = options[:id]
+      @code = @rails = @resque = []
+      if options[:apps]
+        @code = options[:apps]['code'].split ',' if options[:apps]['code']
+        @rails = options[:apps]['rails'].split ',' if options[:apps]['rails']
+        @resque = options[:apps]['resque'].split ',' if options[:apps]['resque']
+      end
+    end
+
+    def apps
+      # delete this once the call has been removed from the template
+      @apps = []
+      @apps
+    end
+  end
+
   class Muxinate < Thor
     require 'json'
     include Thor::Actions
