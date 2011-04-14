b629a6220a13e5fd5fb413599c0e34ad8b09e021
Chris Apolzon <apolzon@gmail.com>
2011-04-13 21:52:48 -0700

    Copied over changes in muxinate.rb's muxinator

--- lib/muxilla/muxinator.rb
+++ lib/muxilla/muxinator.rb
@@ -1,14 +1,14 @@
 module Muxilla
   class Muxinator < Thor::Group
     include Thor::Actions
-    argument :mux, :required => false
+    argument :mux
 
     def self.source_root
-      File.dirname __FILE__
+      `echo $GEM_HOME`.chomp + "/gems/muxilla-0.0.1/lib/muxilla"
     end
 
     def create_tmux_config
-      template 'templates/tmux_config.tt', 'output/tmux_config.tmux', :verbose => false
+      template 'templates/tmux_config.tt', '~/tmux_config.tmux', :verbose => false
     end
   end
 end
